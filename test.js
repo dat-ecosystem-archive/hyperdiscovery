97c0f249750ab84e23bbf6bf3c28b2d0b2e0fd90
karissa <krmckelv@gmail.com>
2016-11-15 16:13:11 -0800

    add failing test for utp

--- test.js
+++ test.js
@@ -6,13 +6,46 @@ var swarm = require('.')
 var core1 = hypercore(memdb())
 var core2 = hypercore(memdb())
 
-tape('connect and close', function (t) {
-  t.plan(8)
-
+function getSwarms (opts) {
   var feed1 = core1.createFeed()
   var feed2 = core2.createFeed(feed1.key)
-  var write = swarm(feed1)
-  var read = swarm(feed2)
+  var write = swarm(feed1, opts)
+  var read = swarm(feed2, opts)
+  return [write, read]
+}
+
+tape('connect and close', function (t) {
+  t.plan(8)
+  var swarms = getSwarms()
+
+  var write = swarms[0]
+  var read = swarms[1]
+
+  write.once('connection', function (peer, type) {
+    t.ok(1, 'write connected')
+    t.equals(write.connections.length, 1)
+    write.close(function () {
+      t.ok(1, 'write closed')
+      read.close(function () {
+        t.equals(write.connections.length, 0)
+        t.equals(read.connections.length, 0)
+        t.ok(1, 'read closed')
+      })
+    })
+  })
+
+  read.once('connection', function (peer, type) {
+    t.ok(1, 'read connected')
+    t.equals(read.connections.length, 1)
+  })
+})
+
+tape('connect without utp', function (t) {
+  t.plan(8)
+  var swarms = getSwarms({utp: false})
+
+  var write = swarms[0]
+  var read = swarms[1]
 
   write.once('connection', function (peer, type) {
     t.ok(1, 'write connected')
